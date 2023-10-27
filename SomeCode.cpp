9280af00ef737b378058bf0a86d89b98963863e4
Marwan Sallam <marwansallam88@gmail.com>
2023-10-27 23:25:43 +0200

    Version 3 with new feture getMin function

--- SomeCode.cpp
+++ SomeCode.cpp
@@ -12,11 +12,23 @@ int getAverage(vector<int> elements){
     return accumulate(elements.begin(),elements.end(),0)/elements.size();
 }
 
+int getMin(vector<int> elements){
+    int resultMin = elements[0];
+    for(int position = 1; elements.size() > position; position++){
+        if(resultMin>elements[position]){
+            resultMin = elements[position];
+        }
+    }
+    return resultMin;
+}
+
 int main() {
     vector<int> v = {0,1,2,3,4,5,6,7,8,9};
     cout << "Sum of all the elements are:  "
         << getSum(v) << endl;
     cout << "Average of all the elements are:  "
         << getAverage(v) << endl;
+    cout << "Minimum of all the elements are:  "
+        << getMin(v) << endl;
    return 0;
 }
